f818868598936b64c0eb37495c774d5c5da15b4e
VladimirP <postriganvova@gmail.com>
2020-05-07 12:20:14 +0300

    Merge branch 'master' of https://github.com/vpostrigan/git_study
    # Conflicts:
    #	src/main/java/com/test/Person.java

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,7 +1,45 @@
 package com.test;
 
-public class Test {
-////
+import javax.net.ssl.SSLContext;
 
+public class Test {
+    public static final String S = "";
+    public final String ss = "";
+
+    static {
+        System.setProperty("file.encoding", "UTF-8");
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("networkaddress.cache.ttl", "300"); // seconds
+        System.setProperty("networkaddress.cache.negative.ttl", "5");
+
+        System.setProperty("http.keepAlive", "true");
+        System.setProperty("http.maxConnections", "4");
+
+        System.setProperty("sun.net.http.errorstream.enableBuffering", "true");
+        // the timeout (in millisec) waiting the error stream to be buffered; default is 300 ms
+        // ErrorStream isn't used, changed to 10
+        System.setProperty("sun.net.http.errorstream.timeout", /* "300" */"10");
+        System.setProperty("sun.net.http.errorstream.bufferSize", "4096");
+
+        // -Djavax.net.ssl.sessionCacheSize=1000
+        System.setProperty("javax.net.ssl.sessionCacheSize", "50000");
+        // 0.5 hour
+        System.setProperty("javax.net.ssl.sessionCacheTimeout", "" + (60 * 30));
+        try {
+            SSLContext sslContext = SSLContext.getDefault();
+            sslContext.getServerSessionContext().setSessionTimeout((60 * 30));
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        // //
+
+        System.out.println(S);
+        System.out.println(new Test().ss);
+    }
+
+    public static void init() {
+        // init
+    }
 
 }
