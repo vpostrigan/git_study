6eae2058462e6a1753f4ab62816f4ea3c9d6ff19
VladimirP <postriganvova@gmail.com>
2020-07-12 23:48:10 +0300

    My commit

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(args);
+        if (true) {
+
+        } else {
+            if (true) {
+
+            } else {
+
+            }
+        }
+    }
+
+}
--- src/main/java/com/test/Person.java
+++ src/main/java/com/test/Person.java
@@ -2,4 +2,5 @@ package com.test;
 
 public class Person {
     //test3 test1
+	//test2
 }
