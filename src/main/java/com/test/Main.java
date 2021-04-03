55a18eaf18d9aeac7d73ab86bba29fbb62753e38
VladimirP <postriganvova@gmail.com>
2021-04-03 17:13:08 +0300

    Emulate rebase

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -3,7 +3,7 @@ package com.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args);
+        System.out.println(args + "2");
         if (true) {
 
         } else {
